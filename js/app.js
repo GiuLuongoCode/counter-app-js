cfc2f37b937ef1364bd2d667b2a3cf62d7354332
joecarian <giuseppe.luongo98@outlook.it>
2023-06-20 16:08:56 +0200

    Added increment by feature

--- js/app.js
+++ js/app.js
@@ -1,18 +1,22 @@
 //This is the main script to manage the counter 
 
 let count = 0;
+let countIncrementBy = 2;
 const add = document.querySelector("#add");
 const btnDecrease = document.getElementById('minus');
 const value = document.querySelector("#value");
 const resetBtn = document.querySelector("#reset");
+const incrementBy = document.querySelector("#incrementLabel");
+const addIncrementBtn = document.getElementById("addIncrement");
+const minusIncrementBtn = document.getElementById("minusIncrement");
 
 add.addEventListener("click", function () {
-		count += 1;
+		count += parseInt(incrementBy.textContent);
 		value.textContent = count;
 	});
 
 btnDecrease.addEventListener("click", function () {
-	count -= 1;
+	count -= parseInt(incrementBy.textContent);
 	value.textContent = count;
 });
 
@@ -20,3 +24,16 @@ resetBtn.addEventListener("click", () => {
 	count = 0;
 	value.textContent = count;
 });
+
+addIncrementBtn.addEventListener("click", () => {
+	countIncrementBy += 1;
+	incrementBy.textContent = countIncrementBy; 
+});
+
+minusIncrementBtn.addEventListener("click", () => {
+	if (countIncrementBy > 1){
+		countIncrementBy -= 1;
+		incrementBy.textContent = countIncrementBy; 
+	}
+});
+
